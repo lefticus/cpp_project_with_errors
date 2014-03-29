21b6034b2ad61a3a66f6941488245671a123d250
Jason Turner <jason@emptycrate.com>
2014-03-29 15:37:36 -0600

    Initial commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+The entire point of this project is to generate a various array of warnings and errors for testinga CI system.
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+// This file is distributed under the BSD License.
+// See "license.txt" for details.
+// Copyright 2014, Jason Turner (jason@emptycrate.com)
+
+int main()
+{
+  return 'a'
+}
