f3aed5847df7e4a03f269ddbb61768cd7dcf690d
Jason Turner <jason@emptycrate.com>
2014-04-10 11:10:13 -0600

    Add a warning

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,20 @@
 // See "license.txt" for details.
 // Copyright 2014, Jason Turner (jason@emptycrate.com)
 
+class MyClass
+{
+  public:
+    MyClass(int i)
+      : m_i(2)
+    {
+    }
+
+  private:
+    int m_i;
+    int m_i2;
+
+};
+
 int main(int argc, char *argv[])
 {
   return 0;
