41296933e5393739e44ab1612c2da1de3cb0f6f2
Jason Turner <jason@emptycrate.com>
2014-04-11 07:12:53 -0600

    Add parsable errors

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,12 @@
 // See "license.txt" for details.
 // Copyright 2014, Jason Turner (jason@emptycrate.com)
 
+#include <iostream>
+
 int main()
 {
+  std::cout << "Expect 2 errors, one in stderr and one on stdout" << std::endl;
+  std::cerr << "src/main.cpp:" << __LINE__ << ": Odd Error" << std::endl;
+  std::cout << "src/main.cpp:" << __LINE__ << ": Odd Error 2" << std::endl;
   return 'a';
 }
